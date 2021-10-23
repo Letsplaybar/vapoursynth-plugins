3a3bbf2c47f32f0ee803a2315585faee08c5b42b
Letsplaybar <admin@myanime.eu>
2021-10-24 01:52:29 +0200

    set build to shared

--- make_autogen.sh
+++ make_autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ${1}
 ./autogen.sh
-./configure
+./configure --enable-shared
 make -j4
 cd ${2}
--- make_config.sh
+++ make_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ${1}
 chmod +x configure
-./configure
+./configure --enable-shared
 make -j4
 cd ${2}
